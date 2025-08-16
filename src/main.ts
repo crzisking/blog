1028196b8b52d43ece0df651101a83f11a57397d
Jacky.Chen <135211647+crzisking@users.noreply.github.com>
2025-08-16 11:57:51 +0800

    Add markdown preview support with VMdPreview and GitHub theme

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,28 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+// main.js
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+// 引入你所使用的主题 此处以 github 主题为例
+import githubTheme from '@kangc/v-md-editor/lib/theme/github';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+// highlightjs
+import hljs from 'highlight.js';
 
 import App from './App.vue'
 import router from './router'
+
+VMdPreview.use(githubTheme, {
+    Hljs: hljs,
+});
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Antd)
+app.use(VMdPreview);
 
 app.mount('#app')
