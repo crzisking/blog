2c3c03e46d78678a4608c474020b8401db38389f
Jacky.Chen <135211647+crzisking@users.noreply.github.com>
2025-08-17 14:35:12 +0800

    Add HeaderView component and update layout structure in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import HeaderView from "@/views/Header/HeaderView.vue";
+import {type CSSProperties} from "vue";
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
 </script>
 
 <template>
-  <RouterView />
+  <a-layout>
+    <a-layout-header :style="headerStyle" >
+      <HeaderView />
+    </a-layout-header>
+    <a-layout-content >
+      111
+      <RouterView />
+    </a-layout-content>
+    <a-layout-footer >Footer</a-layout-footer>
+  </a-layout>
 </template>
 
 <style scoped>
+
 </style>
